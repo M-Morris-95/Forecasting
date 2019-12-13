a1dcfbaa580c3bcab935d7d7340a43cc2e33a260
Michael <michael.morris.london@gmail.com>
2019-12-13 10:27:44 +0000

    initial stationary commit

--- ILI_Forecast.py
+++ ILI_Forecast.py
@@ -4,7 +4,7 @@ import numpy as np
 import datetime as dt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.layers import LSTM, Dropout, Conv1D
 import tensorflow_probability as tfp
 from scipy.stats import pearsonr
@@ -53,75 +53,91 @@ def load_ili_data(path):
     ili_data = ili_data[1]
     return ili_data
 
+def pearson(y_true, y_pred):
+    return tfp.stats.correlation(y_true, y_pred)
+
+def rmse(y_true, y_pred):
+    return tf.sqrt(tf.reduce_mean(tf.square(y_true-y_pred)))
+
 
 timestamp = time.strftime('%b-%d-%Y-%H-%M', time.localtime())
 
 plt.figure(1, figsize=(8, 6), dpi=200, facecolor='w', edgecolor='k')
 plt.figure(2, figsize=(8, 6), dpi=200, facecolor='w', edgecolor='k')
 plt.figure(3, figsize=(8, 6), dpi=200, facecolor='w', edgecolor='k')
+lag = 28
+
+# ili_train - 3265      2005-08-24 ___ 2005-09-20 ___ 2014-08-02
+# +7 days
+# google_train - 3265   2005-08-31 ___ 2005-09-27 ___ 2014-08-09
+# +14 Days
+# y_train - 3238                       2005-10-11 ___ 2014-08-23
+
 
 
 
 
 fold_num = 1
-for fold_num in range(1,5):
+for fold_num in range(1,2):
     data_dir = '/Users/michael/Documents/ili_data/dataset_forecasting_lag28/eng_smoothed_14/fold'+str(fold_num) + '/'
 
     google_train = load_google_data(data_dir + 'google-train')
-    google_train['ili'] = load_ili_data(data_dir + 'ili-train').values
-
-    google_test = load_google_data(data_dir + 'google-test')
-    google_test['ili'] = load_ili_data(data_dir + 'ili-test').values
-
+    ili_in_train = load_ili_data(data_dir + 'ili-train')
     y_train = pd.read_csv(data_dir + 'y-train', header=None)
-    y_test = pd.read_csv(data_dir + 'y-test', header=None)
+    # google_train['ili'] = load_ili_data(data_dir + 'ili-train').values
+
+    all_ili = np.concatenate([ili_in_train[:48], y_train[1]])
+    stationary = all_ili - np.concatenate([np.zeros((1)), all_ili[:-1]])
+    stationary = stationary[28:]
+
+    
 
     n = normalizer(google_train, y_train)
     google_train = n.normalize(google_train, y_train)
+
+    google_input = []
+    ili_input = []
+    ili_target = []
+
+    for i in range(len(google_train)-lag+1):
+        google_input.append(np.asarray(google_train[i:i+lag]))
+        ili_input.append(np.asarray(google_train[i:i + lag]))
+
+    for i in range(len(y_train)-lag+1):
+        google_input.append(np.asarray(google_train[i:i+lag]))
+
+
+    # google_test = load_google_data(data_dir + 'google-test')
+    # ili_in_test = load_ili_data(data_dir + 'ili-test')
+    # y_test = pd.read_csv(data_dir + 'y-test', header=None)
+    # # google_test['ili'] = load_ili_data(data_dir + 'ili-test').values
+
+    #  x_test = []
+
     google_test = n.normalize(google_test, y_test)
 
-    x_train = []
-    x_test = []
-    lag = 28
 
-    for i in range(len(google_train)-lag):
-        x_train.append(np.asarray(google_train[i:i+lag]))
 
-    for i in range(len(google_test)-lag):
-        x_test.append(np.asarray(google_test[i:i+lag]))
 
-    y_train_index = y_train[0][:len(x_train)]
+
+
+
+    # for i in range(len(google_test)-lag+1):
+    #     x_test.append(np.asarray(google_test[i:i+lag]))
+
+    y_train_index = y_train[0]
     x_train, y_train = np.asarray(x_train), np.asarray(y_train[1])
-    y_train = y_train[:x_train.shape[0]]
 
-    y_test_index = y_test[0][:len(x_test)]
+    y_test_index = y_test[0]
     x_test, y_test = np.asarray(x_test), np.asarray(y_test[1])
-    y_test = y_test[:x_test.shape[0]]
-
-    def pearson(y_true, y_pred):
-        return tfp.stats.correlation(y_true, y_pred)
-
-    def rmse(y_true, y_pred):
-        return tf.sqrt(tf.reduce_mean(tf.square(y_true-y_pred)))
 
 
-    # model = Sequential()
-    # model.add(LSTM(128, activation='relu', input_shape=x_train[1].shape[0:2]))
-    # # model.add(LSTM(128, activation='relu', return_sequences=True, input_shape=x_train[1].shape[0:2]))
-    # # model.add(Dropout(0.5))
-    # # model.add(LSTM(128, activation='relu', return_sequences=True))
-    # # model.add(Dropout(0.5))
-    # # model.add(LSTM(128, activation='relu'))
-    # # model.add(Dropout(0.5))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(1, activation='linear'))
 
     model = Sequential()
     model.add(Conv1D(32, 3, input_shape=x_train[1].shape[0:2]))
     model.add(LSTM(128, activation='relu'))
     model.add(Dense(128, activation='relu'))
     model.add(Dense(1, activation='linear'))
-
 
     optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0005, rho=0.9)
 
@@ -131,82 +147,88 @@ for fold_num in range(1,5):
 
 
 
-    model.fit(x_train, y_train,
-              batch_size=16,
-              validation_data=(x_test, y_test),
-              epochs=50,
-              verbose=1)
-
-    model.save_weights('model.hdf5')
-
-
-
-    save_dir = '/Users/michael/Documents/github/Forecasting/Logging/' + timestamp + '/Fold_'+str(fold_num)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    os.chdir(save_dir)
-    training_stats = pd.DataFrame(model.history.history)
-    training_stats.to_csv(r'Fold_'+str(fold_num)+'_training_stats.csv')
-
-    plt.figure(1)
-    plt.subplot(2, 2, fold_num)
-    plt.plot(training_stats.mae)
-    plt.plot(training_stats.val_mae)
-    plt.grid(b=True, which='major', color='#666666', linestyle='-')
-    plt.minorticks_on()
-    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+    # model.fit(x_train, y_train,
+    #           batch_size=16,
+    #           validation_data=(x_test, y_test),
+    #           epochs=50,
+    #           verbose=1)
+    #
+    # model.save_weights('model.hdf5')
 
 
-    plt.figure(2)
-    plt.subplot(2, 2, fold_num)
-    train_pred = model.predict(x_train)
-    plt.plot(pd.to_datetime(y_train_index), train_pred)
-    plt.plot(pd.to_datetime(y_train_index), y_train)
-    plt.legend(['prediction', 'true'])
-    plt.grid(b=True, which='major', color='#666666', linestyle='-')
-    plt.minorticks_on()
-    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
 
 
-    plt.figure(3)
-    plt.subplot(2, 2, fold_num)
-    test_pred = model.predict(x_test)
-    plt.plot(pd.to_datetime(y_test_index), test_pred)
-    plt.plot(pd.to_datetime(y_test_index), y_test)
-    plt.gcf().autofmt_xdate()
-    plt.legend(['prediction', 'true'])
-    plt.grid(b=True, which='major', color='#666666', linestyle='-')
-    plt.minorticks_on()
-    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
 
+########################################################################################################################
+#######################                                 PLOTTING                                 #######################
+########################################################################################################################
 
-    if not 'Best_Results' in locals():
-        temp = pd.DataFrame(training_stats.iloc[np.argmin(training_stats.val_loss.values)])
-        temp = temp.rename(columns={temp.columns[0]: 'Fold'+ str(fold_num)})
-        Best_Results = temp
-    else:
-        temp = pd.DataFrame(training_stats.iloc[np.argmin(training_stats.val_loss.values)])
-        temp = temp.rename(columns={temp.columns[0]: 'Fold' + str(fold_num)})
-        Best_Results = pd.concat([Best_Results,temp], axis=1)
-
-os.chdir('/Users/michael/Documents/github/Forecasting/Logging/' + timestamp)
-Best_Results.to_csv(r'Fold_'+str(fold_num)+'_best_stats.csv')
-
-plt.figure(1)
-plt.savefig('training_stats.png')
-
-
-plt.figure(2)
-plt.savefig('training_predictions.png')
-
-
-plt.figure(3)
-plt.savefig('validation_predictions.png')
-
-plt.figure(1)
-plt.show()
-plt.figure(2)
-plt.show()
-plt.figure(3)
-plt.show()
+#     save_dir = '/Users/michael/Documents/github/Forecasting/Logging/' + timestamp + '/Fold_'+str(fold_num)
+#     if not os.path.exists(save_dir):
+#         os.makedirs(save_dir)
+#
+#     os.chdir(save_dir)
+#     training_stats = pd.DataFrame(model.history.history)
+#     training_stats.to_csv(r'Fold_'+str(fold_num)+'_training_stats.csv')
+#
+#     plt.figure(1)
+#     plt.subplot(2, 2, fold_num)
+#     plt.plot(training_stats.mae)
+#     plt.plot(training_stats.val_mae)
+#     plt.grid(b=True, which='major', color='#666666', linestyle='-')
+#     plt.minorticks_on()
+#     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+#
+#
+#     plt.figure(2)
+#     plt.subplot(2, 2, fold_num)
+#     train_pred = model.predict(x_train)
+#     plt.plot(pd.to_datetime(y_train_index), train_pred)
+#     plt.plot(pd.to_datetime(y_train_index), y_train)
+#     plt.legend(['prediction', 'true'])
+#     plt.grid(b=True, which='major', color='#666666', linestyle='-')
+#     plt.minorticks_on()
+#     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+#
+#
+#     plt.figure(3)
+#     plt.subplot(2, 2, fold_num)
+#     test_pred = model.predict(x_test)
+#     plt.plot(pd.to_datetime(y_test_index), test_pred)
+#     plt.plot(pd.to_datetime(y_test_index), y_test)
+#     plt.gcf().autofmt_xdate()
+#     plt.legend(['prediction', 'true'])
+#     plt.grid(b=True, which='major', color='#666666', linestyle='-')
+#     plt.minorticks_on()
+#     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+#
+#
+#     if not 'Best_Results' in locals():
+#         temp = pd.DataFrame(training_stats.iloc[np.argmin(training_stats.val_loss.values)])
+#         temp = temp.rename(columns={temp.columns[0]: 'Fold'+ str(fold_num)})
+#         Best_Results = temp
+#     else:
+#         temp = pd.DataFrame(training_stats.iloc[np.argmin(training_stats.val_loss.values)])
+#         temp = temp.rename(columns={temp.columns[0]: 'Fold' + str(fold_num)})
+#         Best_Results = pd.concat([Best_Results,temp], axis=1)
+#
+# os.chdir('/Users/michael/Documents/github/Forecasting/Logging/' + timestamp)
+# Best_Results.to_csv(r'Fold_'+str(fold_num)+'_best_stats.csv')
+#
+# plt.figure(1)
+# plt.savefig('training_stats.png')
+#
+#
+# plt.figure(2)
+# plt.savefig('training_predictions.png')
+#
+#
+# plt.figure(3)
+# plt.savefig('validation_predictions.png')
+#
+# plt.figure(1)
+# plt.show()
+# plt.figure(2)
+# plt.show()
+# plt.figure(3)
+# plt.show()
