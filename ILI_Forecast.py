7c6f05c71ec83b21c871dd66ed2c95a2ce465b83
Michael <michael.morris.london@gmail.com>
2019-12-16 12:33:46 +0000

    added server option

--- ILI_Forecast.py
+++ ILI_Forecast.py
@@ -3,14 +3,16 @@ import os
 import numpy as np
 import datetime as dt
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Input, concatenate
 from tensorflow.keras.layers import LSTM, Dropout, Conv1D
+
 import tensorflow_probability as tfp
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import time
+tf.random.set_seed(0)
 
 class normalizer:
     def __init__(self, x_train, y_train):
@@ -43,6 +45,31 @@ class normalizer:
         Y[1] = y_val
         return Y
 
+import argparse
+
+
+def GetParser():
+    parser = argparse.ArgumentParser(
+        description='M-Morris-95 Foprecasting')
+
+    parser.add_argument('--Server', '-S',
+                        type=bool,
+                        help='is it on the server?',
+                        default=False,
+                        required = False)
+
+    return parser
+
+parser = GetParser()
+args = parser.parse_args()
+
+
+def pearson(y_true, y_pred):
+    return tfp.stats.correlation(y_true, y_pred)
+
+def rmse(y_true, y_pred):
+    return tf.sqrt(tf.reduce_mean(tf.square(y_true-y_pred)))
+
 def load_google_data(path):
     google_data = pd.read_csv(path)
     google_data = google_data.drop(['Unnamed: 0'], axis=1)
@@ -65,7 +92,11 @@ plt.figure(3, figsize=(8, 6), dpi=200, facecolor='w', edgecolor='k')
 
 fold_num = 1
 for fold_num in range(1,5):
-    data_dir = '/Users/michael/Documents/ili_data/dataset_forecasting_lag28/eng_smoothed_14/fold'+str(fold_num) + '/'
+
+    if not args.Server:
+        data_dir = '/Users/michael/Documents/ili_data/dataset_forecasting_lag28/eng_smoothed_14/fold'+str(fold_num) + '/'
+    else:
+        date_dir = '/home/mimorris/ili_data/dataset_forecasting_lag28/eng_smoothed_14'+str(fold_num) + '/'
 
     google_train = load_google_data(data_dir + 'google-train')
     google_train['ili'] = load_ili_data(data_dir + 'ili-train').values
@@ -98,30 +129,22 @@ for fold_num in range(1,5):
     x_test, y_test = np.asarray(x_test), np.asarray(y_test[1])
     y_test = y_test[:x_test.shape[0]]
 
-    def pearson(y_true, y_pred):
-        return tfp.stats.correlation(y_true, y_pred)
-
-    def rmse(y_true, y_pred):
-        return tf.sqrt(tf.reduce_mean(tf.square(y_true-y_pred)))
 
 
-    # model = Sequential()
-    # model.add(LSTM(128, activation='relu', input_shape=x_train[1].shape[0:2]))
-    # # model.add(LSTM(128, activation='relu', return_sequences=True, input_shape=x_train[1].shape[0:2]))
-    # # model.add(Dropout(0.5))
-    # # model.add(LSTM(128, activation='relu', return_sequences=True))
-    # # model.add(Dropout(0.5))
-    # # model.add(LSTM(128, activation='relu'))
-    # # model.add(Dropout(0.5))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(1, activation='linear'))
+    inputA = Input(shape=[x_train.shape[1],1])
+    x = LSTM(1, activation='relu')(inputA)
+    x = Model(inputs=inputA, outputs=x)
 
-    model = Sequential()
-    model.add(Conv1D(32, 3, input_shape=x_train[1].shape[0:2]))
-    model.add(LSTM(128, activation='relu'))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(1, activation='linear'))
+    inputB = Input(shape=[x_train.shape[1], x_train.shape[2]-1])
+    y = Conv1D(32, 3)(inputB)
+    y = LSTM(128, activation='relu')(y)
+    y = Model(inputs=inputB, outputs=y)
 
+    combined = concatenate([x.output, y.output])
+    z = Dense(128, activation="relu")(combined)
+    z = Dense(1, activation="linear")(z)
+
+    model = Model(inputs=[x.input, y.input], outputs=z)
 
     optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0005, rho=0.9)
 
@@ -129,19 +152,38 @@ for fold_num in range(1,5):
                   loss='mae',
                   metrics=['mae', 'mse', rmse, pearson])
 
+    model.fit(
+        [x_train[:,:,-1, np.newaxis], x_train[:,:,:-1]], y_train,
+        validation_data=([x_test[:,:,-1, np.newaxis], x_test[:,:,:-1]], y_test),
+        epochs=200, batch_size=8)
 
 
-    model.fit(x_train, y_train,
-              batch_size=16,
-              validation_data=(x_test, y_test),
-              epochs=50,
-              verbose=1)
+    # model = Sequential()
+    # model.add(Conv1D(32, 3, input_shape=x_train[1].shape[0:2]))
+    # model.add(LSTM(128, activation='relu'))
+    # model.add(Dense(128, activation='relu'))
+    # model.add(Dense(1, activation='linear'))
+    #
+    #
+    # optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0005, rho=0.9)
+    #
+    # model.compile(optimizer=optimizer,
+    #               loss='mae',
+    #               metrics=['mae', 'mse', rmse, pearson])
+    #
+    # model.fit(x_train, y_train,
+    #           batch_size=16,
+    #           validation_data=(x_test, y_test),
+    #           epochs=20,
+    #           verbose=1)
 
     model.save_weights('model.hdf5')
 
+    if not args.Server:
+        save_dir = '/Users/michael/Documents/github/Forecasting/Logging/' + timestamp + '/Fold_'+str(fold_num)
+    else:
+        save_dir = '/home/mimorris/Forecasting/Logging' + timestamp + '/Fold_'+str(fold_num)
 
-
-    save_dir = '/Users/michael/Documents/github/Forecasting/Logging/' + timestamp + '/Fold_'+str(fold_num)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -160,7 +202,7 @@ for fold_num in range(1,5):
 
     plt.figure(2)
     plt.subplot(2, 2, fold_num)
-    train_pred = model.predict(x_train)
+    train_pred = model.predict([x_train[:,:,-1, np.newaxis], x_train[:,:,:-1]])
     plt.plot(pd.to_datetime(y_train_index), train_pred)
     plt.plot(pd.to_datetime(y_train_index), y_train)
     plt.legend(['prediction', 'true'])
@@ -171,7 +213,7 @@ for fold_num in range(1,5):
 
     plt.figure(3)
     plt.subplot(2, 2, fold_num)
-    test_pred = model.predict(x_test)
+    test_pred = model.predict([x_test[:,:,-1, np.newaxis], x_test[:,:,:-1]])
     plt.plot(pd.to_datetime(y_test_index), test_pred)
     plt.plot(pd.to_datetime(y_test_index), y_test)
     plt.gcf().autofmt_xdate()
@@ -190,7 +232,12 @@ for fold_num in range(1,5):
         temp = temp.rename(columns={temp.columns[0]: 'Fold' + str(fold_num)})
         Best_Results = pd.concat([Best_Results,temp], axis=1)
 
-os.chdir('/Users/michael/Documents/github/Forecasting/Logging/' + timestamp)
+
+if not args.Server:
+    os.chdir('/Users/michael/Documents/github/Forecasting/Logging/' + timestamp)
+else:
+    os.chdir('/home/mimorris/Forecasting/Logging' + timestamp)
+
 Best_Results.to_csv(r'Fold_'+str(fold_num)+'_best_stats.csv')
 
 plt.figure(1)
