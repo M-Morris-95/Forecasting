5eb72b69b11ef52da3664615046827ed62fb8502
Michael <michael.morris.london@gmail.com>
2020-01-09 11:47:25 +0000

    tidied up code a bit

--- ILI_Forecast.py
+++ ILI_Forecast.py
@@ -434,37 +434,7 @@ else:
     logging_dir = '/home/mimorris/Forecasting/Logging'
     data_dir = '/home/mimorris/ili_data/dataset_forecasting_lag28/eng_smoothed_14/fold'
 
-def predict(series):
-
-    sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-
-    output = tf.expand_dims(START_TOKEN, 0)
-
-    for i in range(MAX_LENGTH):
-        predictions = model(inputs=[sentence, output], training=False)
-
-        # select the last word from the seq_len dimension
-        predictions = predictions[:, -1:, :]
-        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-        # return the result if the predicted_id is equal to the end token
-        if tf.equal(predicted_id, END_TOKEN[0]):
-            break
-
-        # concatenated the predicted_id to the output which is given to the decoder as its input.
-        output = tf.concat([output, predicted_id], axis=-1)
-
-    return tf.squeeze(output, axis=0)
-
-def predict(sentence):
-    prediction = evaluate(sentence)
-    predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
-    return predicted_sentence
-
-
-
-
-for fold_num in range(1,2):
+for fold_num in range(1,5):
     fold_dir = data_dir + str(fold_num) + '/'
     save_dir = logging_dir + timestamp + '/Fold_' + str(fold_num)
     if not os.path.exists(save_dir):
@@ -513,11 +483,11 @@ for fold_num in range(1,2):
     model.load_weights('transformer.hdf5')
     x_train = x_train.swapaxes(1, 2)
     x_test = x_test.swapaxes(1, 2)
-    # model.fit(
-    #     [x_train, teacher_train], y_train,
-    #     validation_data=([x_test, teacher_test], y_test),
-    #     epochs=100, batch_size=64)
-    # model.save_weights('transformer.hdf5')
+    model.fit(
+        [x_train, teacher_train], y_train,
+        validation_data=([x_test, teacher_test], y_test),
+        epochs=100, batch_size=64)
+    model.save_weights('transformer.hdf5')
 
     prediction = np.zeros((y_test[:, -1].shape))
     batch_size = 64
